cbd292f9f58b5b3d1aade971705f74c067451834
mkpark <mkpark@medialog.co.kr>
2025-02-03 17:16:56 +0900

    ConsumerRecord 타입으로 반환 메서드 추가, Kafka Test 라이브러리 추가

--- src/main/java/com/mk/basic/biz/kafka/KafkaConsumer.java
+++ src/main/java/com/mk/basic/biz/kafka/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.mk.basic.biz.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,10 @@ public class KafkaConsumer {
     @KafkaListener(topics = "test-topic", groupId = "test-group")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void consume(ConsumerRecord<String, String> record) {
+        System.out.println("Received message: " + record.value());
     }
 }
